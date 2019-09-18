ff425340dfb5afbc8465524e952a3bb6f86b09a6
neokkk <seven3126@gmail.com>
2019-09-18 18:55:17 +0900

    update

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import Header from './Header';
 import Menu from './Menu';
-import '../pages/index.scss';
 
+import '../../pages/index.scss';
 import './Layout.scss';
 
 const Layout = ({ children }) => {
