1486c620bcbb6ba020797687292b496447910fd9
neokkk <seven3126@gmail.com>
2019-09-18 19:27:16 +0900

    update

--- src/pages/post.js
+++ src/pages/post.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-import Layout from '../components/layout';
+import Layout from '../components/Layout';
 import Post from '../components/Post';
 
 import './index.scss';
