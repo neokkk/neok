fb444c5dde1ea96cb7acc320d37e25673b1efeee
neokkk <seven3126@gmail.com>
2019-10-09 18:01:47 +0900

    posting

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ module.exports = {
   siteMetadata: {
     title: 'neok\'s blog',
     description: 'JavaScript Developer neok\'s dev blog.',
-    keywords: '자바스크립트, JavaScript, React, Node.js, devlog, algorithm',
+    keywords: '자바스크립트, 개발 블로그, 알고리즘, JavaScript, React, Node.js, devlog, algorithm',
     url: 'http://neok.netlify.com/post',
-    image: 'images/ball.png',
+    image: 'http://neok.netlify.com/images/ball.png',
     rights: '©2019 neok All rights reserved.',
   },  
   plugins: [
