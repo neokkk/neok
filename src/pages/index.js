df4d244874da0820e04419c5f6a3b2179b95aac1
neokkk <seven3126@gmail.com>
2019-11-20 15:02:02 +0900

    update effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Layout from '../components/Layout';
 
 import './index.scss';
 
 const IndexPage = () => {
+    const ment = '자바스크립트 개발자 neo의 블로그입니다.';
+    const [dynamic, setDynamic] = useState('');
+
+    let i = 0;
+
+    useEffect(() => {
+        setInterval(() => {
+            if (i > ment.length) {
+                i = i % ment.length;
+            }
+
+            setDynamic(ment.slice(0, i));
+            i++;
+        }, 200);
+    }, []);
+    
     return (
         <div className='index'>
             <Layout>
                 <div className='index-child'>
-                    <p>JavaScript 개발자 neo의 블로그입니다.</p>
+                    <p>{dynamic}</p>
                 </div>
             </Layout>
         </div>
