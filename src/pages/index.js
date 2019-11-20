7c4d6ba43d89fef323a621af124e81f2252c83fe
neokkk <seven3126@gmail.com>
2019-11-20 15:07:27 +0900

    update effect

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,8 +5,8 @@ import Layout from '../components/Layout';
 import './index.scss';
 
 const IndexPage = () => {
-    const ment = '자바스크립트 개발자 neo의 블로그입니다.';
-    const [dynamic, setDynamic] = useState('');
+    const ment = '자바스크립트 개발자 neo의 블로그입니다.   ';
+    const [moving, setMoving] = useState('');
 
     let i = 0;
 
@@ -16,16 +16,16 @@ const IndexPage = () => {
                 i = i % ment.length;
             }
 
-            setDynamic(ment.slice(0, i));
+            setMoving(ment.slice(0, i));
             i++;
-        }, 200);
+        }, 150);
     }, []);
     
     return (
         <div className='index'>
             <Layout>
                 <div className='index-child'>
-                    <p>{dynamic}</p>
+                    <p>{moving}</p>
                 </div>
             </Layout>
         </div>
